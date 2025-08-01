f6561f74b55a348630684d1fd7c07b910320a189
AgroTécnico Chillan <agrotecnicochillan@gmail.com>
2025-08-01 15:49:57 +0000

    fix: Se corrige la ruta principal del proyecto (error 404).

--- mi_almacen/urls.py
+++ mi_almacen/urls.py
@@ -20,5 +20,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path('clients/', include('mi_App.urls')),
+    path('', include('mi_App.urls')),
 ]
